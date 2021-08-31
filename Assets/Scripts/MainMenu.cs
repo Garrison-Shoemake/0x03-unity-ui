9f551e47813d8b657fc3e9ca2e4b839389faa6e6
Garrison-Shoemake <69227891+Garrison-Shoemake@users.noreply.github.com>
2021-08-30 21:16:30 -0500

    A WORKING MENU

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame ()
+	public void PlayMaze()
 	{
 		SceneManager.LoadScene("maze");
+	}
+
+	public void QuitMaze()
+	{
+		Debug.Log("Quit Game");
 	}
 }
